15f848dffc7351d7265f9ec3efdb7d582dfcb2e8
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2025-03-09 10:39:50 +0530

    app

--- app/routes/search.py
+++ app/routes/search.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.database import get_db
-from app.models import Business, BusinessEmbedding
+from database import get_db
+from models import Business, BusinessEmbedding
 from app.config import embedding_model  # Use preloaded model
 import numpy as np
 import json
